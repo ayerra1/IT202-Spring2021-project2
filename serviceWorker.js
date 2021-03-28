42b47f7fe3f77e35b81073548febe5a7673228df
Anurag Reddy Yerrabotula <ayerra4@uic.edu>
2021-03-28 03:09:41 +0000

    updated it to be as installable game

---
+++ serviceWorker.js
@@ -0,0 +1,27 @@
+const cacheName = "project2"
+const assets = [
+    "./",
+    "./index.html",
+    "./gameImages/aeroplane-plane.png",
+    "./gameImages/ali.png",
+    "./gameImages/dia.png",
+    "./gameImages/space1.jpg",
+    "./gameImages/ufo.jpg"
+    
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
